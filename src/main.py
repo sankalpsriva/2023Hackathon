553824bbff372e07ad7a7f97f47e47c8da2a62b5
Diflic <70065767+Diflic@users.noreply.github.com>
2023-01-20 23:22:38 -0500

    Added something like a button task bar

--- src/main.py
+++ src/main.py
@@ -6,12 +6,25 @@ import numbers
 
 
 
-hit = int(100)
+hit = 700
+wit = 100
+i = 0 
 
-def widht(): 
-    global hit 
-    hit = int(10) 
-    button.place(anchor=NW, width=10, height=hit)
+#changes the width dependent on the clicks
+def widht():
+    
+    global wit, i 
+    
+    if i < 1:
+        wit = int(10)
+        button.place(anchor=NW, width=wit, height=hit)
+        i = 1
+    elif i > 0:
+        wit = int(100)
+        button.place(anchor=NW, width=wit, height=hit)
+        i = 0
+    
+    
     
     
 root = Tk() 
@@ -21,7 +34,10 @@ root.title(constants.title)
 root.config(bg = rgbToColor(constants.rgb)) 
 
 button = Button(root , command = widht)
-button.place(anchor=NW, width=10, height=hit)
+button.place(anchor=NW, width=wit, height=hit)
+
+settings = Button(root , command = widht)
+settings.place(anchor=NW, width=10, height=10)
 
 
 
