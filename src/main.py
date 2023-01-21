787eded1312b42d81190661b35204819e800a1b6
sankalpsriva <ssankalp312@gmail.com>
2023-01-20 23:12:26 -0500

    json file updates

--- src/main.py
+++ src/main.py
@@ -1,24 +1,14 @@
 from tkinter import * 
 from helper import rgbToColor
+from playsound import playsound 
+
 import random, constants
-import numbers
 
 
 root = Tk() 
 
 root.geometry(constants.screensize)
 root.title(constants.title)
-root.config(bg = rgbToColor(constants.rgb)) 
-
-button = Button(root)
-button.place(anchor=NW, width=10, height=700)
-
-
+root.config(bg = rgbToColor(constants.rgb))     
 
 root.mainloop()
-
-
-
-
-
-
