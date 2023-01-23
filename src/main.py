3ccf9c3e79a95c8b2b6da86110cb0c1553f1ad37
Diflic <70065767+Diflic@users.noreply.github.com>
2023-01-23 10:52:40 -0500

    Settings Image

--- src/main.py
+++ src/main.py
@@ -8,9 +8,11 @@ constants.root.title(constants.title)
 constants.root.config(bg = rgbToColor(constants.color))     
 
 tkinterWidgets.button.place(anchor = NW, width = 10, height = constants.heightOnClick)
-tkinterWidgets.settings.place(anchor = N, relx = 0.5, rely = -0.001, width = 200, height = 200)
+tkinterWidgets.settings.place(anchor = N, relx = 0.3, rely = -0.001, width = 200, height = 200)
 tkinterWidgets.timeLabel.place(anchor = N, relx = 0.5, rely = -0.001)
 tkinterWidgets.mailButton.place(anchor = N, relx = 0.95, rely = 0.05)
+
+
 
 constants.root.after(1000, lambda: labelUpdate(tkinterWidgets.timeLabel))
 constants.root.mainloop()   
