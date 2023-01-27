6de20cdd60b4a9ba1ebfbe0225cc64b1a084d086
Jackson David <97686021+davidjac002@users.noreply.github.com>
2023-01-27 10:21:38 -0500

    Updated buttons to default browser

--- src/tkinterWidgets.py
+++ src/tkinterWidgets.py
@@ -15,10 +15,10 @@ resetButton = Button(constants.root, text = "Reset", command = reset)
 
 mailButton = Button(constants.root, text = "Mail", command = emailMenu, width = 7)
 
-googleButton = Button(constants.root, text = "Google", command = lambda: openWebsite("google.com")) 
-canvasButton = Button(constants.root, text = "Canvas", command = lambda: openWebsite("hse.instructure.com")) 
+googleButton = Button(constants.root, text = "Google", command = lambda: openWebsite("https://www.google.com/")) 
+canvasButton = Button(constants.root, text = "Canvas", command = lambda: openWebsite("https://hse.instructure.com/")) 
 skywardButton = Button(constants.root, text = "Skyward", command = lambda: openWebsite("https://sis.hse.k12.in.us/scripts/wsisa.dll/WService=wsEAplus/seplog01.w")) 
-cleverButton = Button(constants.root, text = "Clever", command = lambda: openWebsite("clever.com")) 
+cleverButton = Button(constants.root, text = "Clever", command = lambda: openWebsite("https://clever.com/")) 
 
 timeLabel = Label(constants.root, text = f"{datetime.now().replace(microsecond=0).strftime('%d-%m-20%y - %I:%M:%S')}", 
             width = 40, wraplength = 500, justify = LEFT, 
